66df879af9222d77ddf365f33eead0ae0b3f4f0a
Kevlarrrr <kevin.lam@ucdconnect.ie>
2024-04-20 23:48:58 +0100

    new modified

--- src/Main.java
+++ src/Main.java
@@ -1,84 +1,48 @@
-import javax.swing.*;
-import java.sql.Time;
 import java.util.Arrays;
 import java.util.Random;
 
 public class Main {
     static Random rand = new Random();
-    static Random seedGen = new Random();
-    private Main(){}
+
+    private Main() {}
 
     public static void PTPSort(Integer[] a, int beg, int end) {
+        if (end <= beg + 1) {
+            return; // Base case for recursion: return if the section to sort is 1 or 0 elements
+        }
 
-        int seed = seedGen.nextInt();
-//        System.out.println("Seed: "+ seed);
-        rand.setSeed(2046273340);
-
-        //-1917837701
-        //-1539768376 index -1 error
-        //66046558 index 100 error
-        //2046273340
-
-        //finding middle index of current subarray
-        int mid = (beg + end)/2;
-
-        //finding different random indexes in subarray
+        // Selecting three distinct indices for pivots
         int iPLo = 0, iPMi = 0, iPHi = 0;
+
+        // Ensure the pivots are distinct
         while ((iPLo == iPMi) || (iPMi == iPHi) || (iPHi == iPLo)) {
             iPLo = Sampling(beg, end);
             iPMi = Sampling(beg, end);
             iPHi = Sampling(beg, end);
         }
 
-
-        // ensure pivots are in correct order
-        if (iPMi < iPLo) {
-            int temp = iPLo;
-            iPLo = iPMi;
-            iPMi = temp;
-        }
-        if (iPHi < iPMi) {
-            int temp = iPMi;
-            iPMi = iPHi;
-            iPHi = temp;
-        }
-
-
-        System.out.printf("\n\niPLo: %d, iPMi: %d, iPHi: %d", iPLo, iPMi, iPHi);
-
-        //finding correspondent values of indexes
+        // we get the pivot values
         int PLo = a[iPLo], PMi = a[iPMi], PHi = a[iPHi];
-//        if (PMi < PLo) {
-//            int temp = PLo;
-//            PLo = PMi;
-//            PMi = temp;
-//        }
-//        if (PHi < PMi) {
-//            int temp = PMi;
-//            PMi = PHi;
-//            PHi = temp;
-//        }
 
-        //finding 3 initial pivots
-        //  NOTE 1 : each will be a different task
-        //  NOTE 2 : iL and iH are temporary and used to find ilm and imh
-        System.out.println("\n1");
-        int iL = Hoare(a, beg, beg+mid-1, PLo);
-        System.out.println("2");
-        int iH = Hoare(a, beg+mid, end, PHi);
-        System.out.println("3");
-        int iM = Hoare(a, iL, iH-1, PMi);
-        System.out.println("4");
-        int ilm = Hoare(a, iL, iM-1, PLo);
-        //recursion here on first 2 subarrays on different threads
-        System.out.println("5");
-        int imh = Hoare(a, iM, iH-1, PHi);
-        //recursion here on last 2 subarrays on different threads
+        // Order pivot values
+        if (PMi < PLo) { int tmp = PMi; PMi = PLo; PLo = tmp; }
+        if (PHi < PMi) { int tmp = PHi; PHi = PMi; PMi = tmp; }
+        if (PMi < PLo) { int tmp = PMi; PMi = PLo; PLo = tmp; }
 
+        System.out.println("Pivots: " + PLo + ", " + PMi + ", " + PHi);
 
-        System.out.printf("\n\nPLo: %d, PMi: %d, PHi: %d", PLo, PMi, PHi);
-        System.out.printf("\niL: %d, iM: %d, iH: %d", iL, iM, iH);
-        System.out.printf("\nilm: %d, imh: %d\n", ilm, imh);
+        // Partitioning the array around each pivot
+        int iL = Hoare(a, beg, beg-1, PLo);
+        int iM = Hoare(a, iL + 1, end, PMi);
+        int iH = Hoare(a, iM + 1, end, PHi);
+
+        System.out.println("Partitions at: " + iL + ", " + iM + ", " + iH);
+
+        // Recursively sorting the partitions
+        PTPSort(a, beg, iL);
+        PTPSort(a, iL + 1, iM);
+        PTPSort(a, iM + 1, iH);
+        PTPSort(a, iH + 1, end);
     }
 
     public static int Hoare(Integer[] a, int b, int c, int P) {
@@ -96,16 +60,16 @@ public class Main {
             if (i > j) {
                 return j;
             }
-                temp = a[i];
-                a[i] = a[j];
-                a[j] = temp;
+            temp = a[i];
+            a[i] = a[j];
+            a[j] = temp;
 
 
         }
     }
 
     public static int Sampling(int beg, int end) {
-        return rand.nextInt(beg, end-1);
+        return rand.nextInt(end - beg) + beg;
     }
 
     public static void main(String[] args) {
@@ -121,19 +85,8 @@ public class Main {
                 33, 86, 52, 54, 16, 81, 73, 44, 90, 85,
                 7, 3, 2, 1, 4, 5, 6, 8, 9, 0
         };
-//                                     [1,2,5,6,8,10,23,43,65,76];
-//        int temp = arr[6];
-//        arr[6] = arr[2];
-//        arr[2] = temp;
-//        for (int i = 0; i < arr.length-1; i++) {
-//            System.out.print(arr[i]+" ");
-//        }
-//        System.out.println(Hoare(arr, 0, arr.length - 1, 23));
 
-        PTPSort(arr, 0, arr.length-1);
-
-
+        PTPSort(arr, 0, arr.length);
+        System.out.println(Arrays.toString(arr)); // Print the sorted array
     }
 }
-
-
